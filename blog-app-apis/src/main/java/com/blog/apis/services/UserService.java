efc0c045f8c91fa8bedfc10a11bbd990d983509a
failure64 <swastikmiddha@gmail.com>
2023-10-13 01:20:25 +0530

    User Services created
    all User Services created hence implementing CRUD functionalities for the user. controllers part left for the user. only post method created

--- blog-app-apis/src/main/java/com/blog/apis/services/UserService.java
+++ blog-app-apis/src/main/java/com/blog/apis/services/UserService.java
@@ -8,7 +8,7 @@ import com.blog.apis.payloads.UserDto;
 public interface UserService {
 	
 	UserDto createUser(UserDto user);
-	UserDto updateUser(UserDto user, Integer userID);
+	UserDto updateUser(UserDto user, Integer userId);
 	UserDto getUserById(Integer userId);
 	List<UserDto> getAllUsers();
 	void deleteUser(Integer userId);
