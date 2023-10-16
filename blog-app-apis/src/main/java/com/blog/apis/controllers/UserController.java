b91c3ddfe4b3b21512d5bdfdabb6fd3f460b574d
failure64 <swastikmiddha@gmail.com>
2023-10-16 13:59:43 +0530

    user api completed
    completed all the user apis. fixed the validation error by adding to tom
    <dependency>
       <groupId>org.springframework.boot</groupId>
       <artifactId>spring-boot-starter-validation</artifactId>
    </dependency>

--- blog-app-apis/src/main/java/com/blog/apis/controllers/UserController.java
+++ blog-app-apis/src/main/java/com/blog/apis/controllers/UserController.java
@@ -1,15 +1,21 @@
 package com.blog.apis.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.blog.apis.payloads.UserDto;
+import com.blog.apis.payloads.ApiResponse;
 import com.blog.apis.services.UserService;
 
 @RestController
@@ -26,5 +32,33 @@ public class UserController {
 		UserDto createUserDto = this.userService.createUser(userDto);
 		return new ResponseEntity<>(createUserDto,HttpStatus.CREATED);
 	}
+	
+	@PutMapping("/{userId}")
+	public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto,@PathVariable("userId") Integer uid)
+	{
+		UserDto updatedUser= this.userService.updateUser(userDto,  uid);
+		return ResponseEntity.ok(updatedUser);
+	}
+	
+	@DeleteMapping("/{userId}")
+	public ResponseEntity<?> deleteUser(@PathVariable("userId") Integer uid)
+	{
+		this.deleteUser(uid);
+//		return new ResponseEntity(Map.of("message","User Deleted Successfully"), HttpStatus.OK);
+		return new ResponseEntity<ApiResponse>(new ApiResponse("USER DELETED SUCCESSFULLY",true ), HttpStatus.OK);
+	}
+	
+	@GetMapping("/")
+	public ResponseEntity<List<UserDto>> getAllUsers()
+	{
+		return ResponseEntity.ok(this.userService.getAllUsers());
+	}
+	
+	@GetMapping("/{userId}")
+	public ResponseEntity<UserDto> getSingleUser(@PathVariable Integer userId)
+	{
+		return ResponseEntity.ok(this.userService.getUserById(userId));
+	}
+
 
 }
--- blog-app-apis/src/main/java/com/blog/apis/services/UserService.java
+++ blog-app-apis/src/main/java/com/blog/apis/services/UserService.java
@@ -2,9 +2,12 @@ package com.blog.apis.services;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 //import com.blog.apis.entites.User;
 import com.blog.apis.payloads.UserDto;
 
+//@Service
 public interface UserService {
 	
 	UserDto createUser(UserDto user);
